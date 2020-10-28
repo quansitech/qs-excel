527c11f92123570955940ac4117d2bf5444184d9
tiderjian <tider@quansitech.com>
2020-10-28 17:53:37 +0800

    优化ListLoader逻辑，避免excel文件存在大量空行时导致出现内存不足的错误

--- src/Loader/ListLoader.php
+++ src/Loader/ListLoader.php
@@ -7,14 +7,32 @@ class ListLoader extends LoaderContract
 {
     public function load($file, $sheet_index){
         $spreadsheet = IOFactory::load($file);
-        $sheetData = $spreadsheet->getSheet($sheet_index)->toArray(null, true, true, true);
+        //$sheetData = $spreadsheet->getSheet($sheet_index)->toArray(null, true, true, true);
 
         $return_list = [];
-         foreach($sheetData as $data){
-             if(array_filter(collect($data)->flatten()->all())){
-                 $return_list[] = $data;
-             }
-         }
-         return $return_list;
+        $worksheet = $spreadsheet->getSheet($sheet_index);
+        foreach($worksheet->getRowIterator() as $row){
+
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false);
+
+            $tmp = [];
+            $break_flag = false;
+            foreach($cellIterator as $cell){
+                if(trim($cell->getValue()) == ''){
+                    $break_flag = true;
+                    break;
+                }
+                $tmp[$cell->getColumn()] = trim($cell->getValue());
+            }
+
+            if($break_flag){
+                break;
+            }
+
+            $return_list[] = $tmp;
+        }
+
+        return $return_list;
     }
 }

d8a34fd446277ad13855eb31c3dde48cf658896b
xhiny <axhiny@163.com>
2021-08-25 17:08:24 +0800

    修复某行首列为空字符串，读取excel剩下行数据失败bug

--- src/Loader/ListLoader.php
+++ src/Loader/ListLoader.php
@@ -11,20 +11,33 @@ class ListLoader extends LoaderContract
 
         $return_list = [];
         $worksheet = $spreadsheet->getSheet($sheet_index);
+        $row_n = 0;
+        $header_n = 0;
         foreach($worksheet->getRowIterator() as $row){
 
             $cellIterator = $row->getCellIterator();
             $cellIterator->setIterateOnlyExistingCells(false);
 
             $tmp = [];
+            $n = 0;
             foreach($cellIterator as $cell){
+                // 表头列为空或者列数为表头数则退出
+                if(($row_n == 0 && trim($cell->getValue()) == '') || ($row_n > 0 && $n === $header_n)){
+                    break;
+                }
+                if ($row_n == 0){
+                    $header_n++;
+                }
+                $n++;
                 $tmp[$cell->getColumn()] = trim($cell->getValue());
             }
 
-            if(empty($tmp)){
+            // 行数据全部为空字符串则退出
+            if(empty(array_filter($tmp, function ($ent){return trim($ent) !== '';}))){
                 break;
             }
 
+            $row_n++;
             $return_list[] = $tmp;
         }
 
